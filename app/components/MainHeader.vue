9c04554108be9f2fe298da6a8452628ef654768f
Vladislav <M1LkMenIO@yandex.ru>
2026-03-08 15:09:33 +0300

    init

---
+++ app/components/MainHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <header class="sticky top-0 z-50 flex items-center justify-center h-12 border-b border-gray-800 bg-slate-900">
+    <img src="/fox.png" alt="fox-icon" class="h-6 w-6 mr-2">
+    <span class="text-xl font-bold">Foxtrot</span>
+  </header>
+</template>
---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  {
+    rules: {
+      'vue/max-attributes-per-line': 'off',
+      '@stylistic/quotes': 'off',
+      '@stylistic/semi': 'off',
+      '@stylistic/comma-dangle': 'off'
+    }
+  }
+)
